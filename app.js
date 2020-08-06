a63a86bc126002e04089d22940e1646bee7f268e
Master Chief <kemar7@live.com>
2020-08-06 11:29:06 -0400

    mid 9.3.5 promises

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
+  
+/*const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 //const name = profileDataArgs[0];          seconde
 //const github = profileDataArgs[1];
-/*console.log(profileDataArgs);
+console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -24,8 +38,10 @@ printProfileData(profileDataArgs);*/
 //console.log(name, github);
 //console.log(generatePage(name, github));      third
 
-fs.writeFile('index.html', generatePage(name, github), err => {
+//4TH COMMENT ALL OUT fsfunc & 3const above
+
+/*fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!')
-});
+});*/
